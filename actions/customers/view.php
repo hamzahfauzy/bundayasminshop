fa45dbeee91a66e243a55f7eed2de6776834f016
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2022-02-11 14:07:57 +0700

    update query

--- actions/customers/view.php
+++ actions/customers/view.php
@@ -7,7 +7,7 @@ $data = $db->single('customers',[
     'id' => $_GET['id']
 ]);
 
-$query = "SELECT transactions.*, SUM(transaction_payments.subtotal) as total_payment FROM transactions JOIN transaction_payments ON transaction_payments.transaction_id = transactions.id WHERE transactions.customer_id = $_GET[id]";
+$query = "SELECT transactions.*, SUM(transaction_payments.subtotal) as total_payment FROM transactions JOIN transaction_payments ON transaction_payments.transaction_id = transactions.id WHERE transactions.customer_id = $_GET[id] GROUP BY transactions.id";
 $db->query = $query;
 $transactions = $db->exec('all');
 
